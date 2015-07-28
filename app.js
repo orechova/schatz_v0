b0d56042b82a9e73fece9df41fd4853422fbd12f
orechova <vlassky.oriesok@yahoo.com>
2015-07-28 16:35:53 +0200

    Adding new words and displaying all

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+var pom;
+$(function(){
 
 	var model = {
 		init: function(){
@@ -11,9 +12,52 @@ $(document).ready(function(){
 			var word = {'textSK': textSK, 'textIT': textIT, 'lastTouched': new Date() }
 			data.push(word);
 			localStorage.schatz01Words = JSON.stringify(data);
+			return true;
 		},
 		getAll: function(){
 			return JSON.parse( localStorage.schatz01Words );
 		}
 	}
-})
+
+	var view = {
+		init: function(){
+			$('#addNewWord').click(function(){
+				if ( controller.addNewWord($('#textSK').val(), $('#textIT').val()) ){
+					$('#textSK').val('');
+					$('#textIT').val('');
+				}
+			});
+			$(document).on("pagebeforeshow","#listWords",function(){
+  				view.displayAllWords();
+			});
+		},
+		displayAllWords: function(){
+			var words = controller.getAllWords();
+			var html = '';
+			console.log(words);
+			pom = words;
+			for (var wordID in words){
+				if (words.hasOwnProperty(wordID)){
+					var word = words[wordID];
+					html += '<tr><td>' + word.textSK + '</td><td>' + word.textIT + '</td></tr>';
+				}
+			}
+			$('#listWordsBody').html(html);
+		}
+	}
+
+	var controller = {
+		init: function(){
+			model.init();
+			view.init();
+		},
+		addNewWord: function(textSK, textIT){
+			return model.add(textSK, textIT);
+		},
+		getAllWords: function(){
+			return model.getAll();
+		}
+	}
+
+	controller.init();
+});
